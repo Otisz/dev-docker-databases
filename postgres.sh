28f10d648ae93cf1549f4ef9bbd80b253177e145
Levente Otta <leventeotta@gmail.com>
2020-08-13 11:01:21 +0200

    Add PGSQL

---
+++ postgres.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+read -p "Remove stack? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	docker stack rm postgres
+
+	until docker stack rm postgres
+	do
+		sleep 1
+	done
+fi
+
+
+
+echo ""
+read -p "Pull? " -n 1 -r
+echo ""
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	docker pull postgres:latest
+fi
+
+
+
+echo ""
+read -p "Deploy? " -n 1 -r
+echo ""
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	docker stack deploy --compose-file ./postgres/stack.yml postgres
+	sleep 2
+	docker service ls
+fi
